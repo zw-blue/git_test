e45ec22a383af785fcbc204eb507b4e3476f8eab
zw-blue <68576048+zw-blue@users.noreply.github.com>
2022-01-13 22:38:32 +0800

    Update GitTest.java

--- src/main/java/com/zhao/GitTest.java
+++ src/main/java/com/zhao/GitTest.java
@@ -3,5 +3,6 @@ package com.zhao;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello, this is a git test!");
+        System.out.println("hello, zhao");
     }
 }
