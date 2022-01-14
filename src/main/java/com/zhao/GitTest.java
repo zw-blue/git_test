91b254cbe84e2ff7732b09243cd842fdc8d51d64
zhaowei <765418108@qq.com>
2022-01-14 10:00:57 +0800

    second commit

--- src/main/java/com/zhao/GitTest.java
+++ src/main/java/com/zhao/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello, this is a git test!");
         System.out.println("hello, zhao");
+        System.out.println(".....");
     }
 }
